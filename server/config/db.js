5b8718a95624d1da8a17949908fa4dc1a42e89af
hedy-kh <hedykhkh@gmail.com>
2025-10-12 21:06:07 +0200

    add db connection

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+exports.ConnectDb=async()=> {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('connection success')
+    } catch (error) {
+        console.log('connection error')
+    }
+}
