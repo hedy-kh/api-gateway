7c5cf587276ad89b852d39ba92b488024c53f9f0
hedy-kh <hedykhkh@gmail.com>
2025-10-18 10:15:07 +0200

    added docker config

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,29 +1,67 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Homepage from './pages/home/Homepage'
-import About from './pages/home/About'
-import Contact from './pages/home/Contact'
-import Hero from './pages/home/Hero'
-import Developer from './pages/home/Developer'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Rest from './pages/auth/Rest'
+import { Suspense, lazy } from 'react'
+
+const Loading = lazy(() => import('./components/Loading'));
+const Homepage = lazy(() => import('./pages/home/Homepage'));
+const About = lazy(() => import('./pages/home/About'))
+const Contact = lazy(() => import('./pages/home/Contact'))
+const Hero = lazy(() => import('./pages/home/Hero'))
+const Developer = lazy(() => import('./pages/home/Developer'))
+const Login = lazy(() => import('./pages/auth/Login'))
+const Register = lazy(() => import('./pages/auth/Register'))
+const Rest = lazy(() => import('./pages/auth/Rest'))
+const Notfound = lazy(() => import('./components/Notfound.jsx'))
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Homepage />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Contact' element={<Contact />} />
-        <Route path='/Hero' element={<Hero />} />
-        <Route path='/Developer' element={<Developer />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path='/Rest' element={<Rest />} />
+        <Route index element={
+          <Suspense fallback={<Loading />}>  {/* ✅ Fixed */}
+            <Homepage />
+          </Suspense>
+        }
+        />
+        <Route path='/About' element={
+          <Suspense fallback={<Loading />}>  {/* ✅ Fixed */}
+            <About />
+          </Suspense>
+        } />
+        <Route path='/Contact' element={
+          <Suspense fallback={<Loading />}>  {/* ✅ Fixed */}
+            <Contact />
+          </Suspense>
+        } />
+        <Route path='/Hero' element={
+          <Suspense fallback={<Loading />}>  {/* ✅ Fixed */}
+            <Hero />
+          </Suspense>
+        } />
+        <Route path='/Developer' element={
+          <Suspense fallback={<Loading />}>  {/* ✅ Fixed */}
+            <Developer />
+          </Suspense>
+        } />
+        <Route path='/Login' element={
+          <Suspense fallback={<Loading />}>
+            <Login />
+          </Suspense>
+        } />
+        <Route path='/Register' element={
+          <Suspense fallback={<Loading />}>
+            <Register />
+          </Suspense>
+        } />
+        <Route path='/Rest' element={
+          <Suspense fallback={<Loading />}>
+            <Rest />
+          </Suspense>
+        } />
+        <Route path='*' element={<Notfound />} />
+        <Route path='/lazy' element={<Loading/>}/>
       </Routes>
+
     </BrowserRouter>
   )
 }
-
 export default App
